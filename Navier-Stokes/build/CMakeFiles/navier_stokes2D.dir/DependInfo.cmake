92d6df838b507aa958e3f651c2cc9eab70993178
Davide Villani <davidevillani2002@gmail.com>
2025-02-08 18:35:37 +0000

    implementazione matrici A M C separate e assemble time step

---
+++ Navier-Stokes/build/CMakeFiles/navier_stokes2D.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/davide-villani/Desktop/PDE/PROJECT/NavierStokes_Project_NM4PDE/Navier-Stokes/src/NavierStokes2D.cpp" "CMakeFiles/navier_stokes2D.dir/src/NavierStokes2D.cpp.o" "gcc" "CMakeFiles/navier_stokes2D.dir/src/NavierStokes2D.cpp.o.d"
+  "/home/davide-villani/Desktop/PDE/PROJECT/NavierStokes_Project_NM4PDE/Navier-Stokes/src/main2D.cpp" "CMakeFiles/navier_stokes2D.dir/src/main2D.cpp.o" "gcc" "CMakeFiles/navier_stokes2D.dir/src/main2D.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Navier-Stokes/build/CMakeFiles/navier_stokes2D.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/navier_stokes2D.dir/src/NavierStokes2D.cpp.o"
+  "CMakeFiles/navier_stokes2D.dir/src/NavierStokes2D.cpp.o.d"
+  "CMakeFiles/navier_stokes2D.dir/src/main2D.cpp.o"
+  "CMakeFiles/navier_stokes2D.dir/src/main2D.cpp.o.d"
+  "navier_stokes2D"
+  "navier_stokes2D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/navier_stokes2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
