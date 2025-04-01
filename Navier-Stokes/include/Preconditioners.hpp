a396ee8c5eb8d8686c10f30135c54d63d14a2a08
root <emanuele1.caruso@mail.polimi.it>
2025-04-01 13:27:19 +0000

    Davide non aveva pushato i preconditioner corretti

--- Navier-Stokes/include/Preconditioners.hpp
+++ Navier-Stokes/include/Preconditioners.hpp
@@ -264,53 +264,35 @@ using namespace dealii;
       const unsigned int maxit = 10000;
       const double tol = 1e-2;
 
-      //devo definire due tmp non posso fare block 
+      tmp.reinit(src); 
 
-
-       // Prepare 2 temporary vectors to hold intermediate data.
-        tmp.reinit(src); 
-
-        // --- Step 1 ---
-        // Solve for the primary (first block) variable.
-        // This computes an approximate inverse of C applied to the first part of src.
+      // Step 1) 
         SolverControl solver_control_F(maxit, tol * src.block(0).l2_norm());
         SolverGMRES<TrilinosWrappers::MPI::Vector> solver_F(solver_control_F);
         solver_F.solve(*F, dst.block(0), src.block(0), preconditioner_F);
 
-        // --- Step 2 ---
-        // Copy the secondary part of src into a temporary container.
-        // Then update this temporary vector by incorporating the effect of -B acting
-        // on the primary variable obtained in Step 1.
-  
+      // Step 2) 
         B->vmult(dst.block(1), dst.block(0));
         dst.block(1).sadd(-1.0, src.block(1)); // tmp.block(1) = -B * dst.block(0) + src.block(1)
         tmp.block(1) = dst.block(1);
 
-        // --- Step 3 ---
-        // Solve the system with the approximate Schur complement.
-        // This computes the secondary variable by inverting negS_matrix.
+      // Step 3)
         SolverControl solver_control_S(maxit, tol * tmp.block(1).l2_norm());
         SolverGMRES<TrilinosWrappers::MPI::Vector> solver_S(solver_control_S);
         solver_S.solve(neg_S, dst.block(1), tmp.block(1), preconditioner_S);
 
-        // --- Step 4 ---
-        // Scale the primary component by the original diagonal entries.
-        // This reintroduces the proper weighting based on C's diagonal.
+      // Step 4)
         dst.block(0).scale(diag_D);
 
-        // --- Step 5 ---
+      // Step 5)
         // Adjust the secondary component by applying the damping factor.
         dst.block(1) /= alpha;
 
-        // --- Step 6 ---
-        // Refine the primary component: subtract a correction term computed by
-        // multiplying the secondary variable with B^T and then scaling with the inverse
-        // of the diagonal entries.
+      // Step 6)
         B_T->vmult(tmp.block(0), dst.block(1));
         dst.block(0) -= tmp.block(0);
         
-        // --- Step 7 ---
-        // Finalize the update of the primary component by scaling it with Dinv.
+      // Step 7)
         dst.block(0).scale(diag_D_inv);
       
     }
@@ -427,116 +409,102 @@ using namespace dealii;
     mutable TrilinosWrappers::MPI::Vector res;
   };
 
-  // Precondition approximate Yosida: Why it so slow?
-  
-  class PreconditionaYosida 
- {
+  // Precondition approximate Yosida
+  class PreconditionaYosida
+  {
   public:
     void
     initialize(const TrilinosWrappers::SparseMatrix &F_,
                const TrilinosWrappers::SparseMatrix &B_,
                const TrilinosWrappers::SparseMatrix &B_t,
                const TrilinosWrappers::SparseMatrix &M_,
-               const TrilinosWrappers::MPI::BlockVector &sol_owned)
+               const TrilinosWrappers::MPI::BlockVector &sol_owned
+               )
     {
       F = &F_;
       B = &B_;
       B_T = &B_t;
       M = &M_;
+     
+      diag_M.reinit(sol_owned.block(0));
+      neg_diag_M_inv.reinit(sol_owned.block(0));
 
-      diag_D_inv.reinit(sol_owned.block(0));
-      diag_D.reinit(sol_owned.block(0));
-      lump_M.reinit(sol_owned.block(0));
-
-
-      for (unsigned int i : diag_D.locally_owned_elements())
+      for (unsigned int i : diag_M.locally_owned_elements())
       {
-        double temp = F->diag_element(i);
-        diag_D[i] = temp;
-        diag_D_inv[i] = 1.0 / temp; // (F_hat)^-1
+        double temp = M->diag_element(i); //no lump for better performance
+        neg_diag_M_inv[i] = - 1.0 / temp;
       }
 
+      //S_tilde = BD(^-1)B.T
+      B->mmult(neg_S, *B_T, neg_diag_M_inv); //Note: we need -S_tilde, so we use - D(^-1)
 
-    // build lumped_M
-    for (unsigned int i : lump_M.locally_owned_elements())
-      {
-        //Note : we have assembled M/deltat
-        double temp = 0.0;
-        for (auto it = M->begin(i); it != M->end(i); ++it)
-            temp += std::abs(it->value());
-
-        
-        lump_M[i] = -1.0 / temp; // - deltat * (lump_M)^-1
-      }
-
-      //Note: We use -lump_M to create negative S
-      B->mmult(negative_S, *B_T, lump_M); // neg_S
-
-      preconditionerF.initialize(*F);
-      preconditionerS.initialize(negative_S);
+      preconditioner_F.initialize(*F);
+      preconditioner_S.initialize(neg_S); //already assembled neg_S
     }
 
     void
     vmult(TrilinosWrappers::MPI::BlockVector &dst,
-          const TrilinosWrappers::MPI::BlockVector &src) const 
-    { 
-      //Note : diag_D_inv = (F_hat)^-1
-      tmp.reinit(src.block(0)); // block 0
-      tmp2.reinit(src.block(1)); //block 1 
+          const TrilinosWrappers::MPI::BlockVector &src) const
+    {
 
-      const unsigned int maxiter = 100000;
+      const unsigned int maxit = 10000;
       const double tol = 1e-2;
 
-      // Store in temporaries the results of src updates 
-      TrilinosWrappers::MPI::Vector yu = src.block(0);
-      TrilinosWrappers::MPI::Vector yp = src.block(1);
 
-      //Step 1) (F_hat)^-1 * src(0) = tmp
-      
-      tmp = src.block(0);
-      tmp.scale(diag_D_inv);
-      yu = tmp;      
+       // Prepare Temp to hold partial results
+        tmp.reinit(src); 
 
-      //Step 2)   
-       B->vmult(tmp2, tmp); //tmp(1) = B*tmp(0)
-       yp.sadd(-1.0,tmp2); // src(1) = -tmp(1) + src(1) (RHS)
-       
-       //Step 3) true solution of neg_S to have better accuracy, instead of neg_S_hat
-      SolverControl solver_S(maxiter, tol * yp.l2_norm());
-      SolverCG<TrilinosWrappers::MPI::Vector> solver_cg(solver_S);
-      solver_cg.solve(negative_S, dst.block(1), yp, preconditionerS); //dst.block(1) updated here 
+        // Step 1) 
+        // This computes an approximate inverse of F applied to the first block of src.
+        SolverControl solver_control_F(maxit, tol * src.block(0).l2_norm());
+        SolverGMRES<TrilinosWrappers::MPI::Vector> solver_F(solver_control_F);
+        solver_F.solve(*F, dst.block(0), src.block(0), preconditioner_F);
 
-      yp = dst.block(1); //updating src(1) for next computations
+        // Step 2) 
+        // Then update this temporary vector by incorporating the effect of -B.
+        B->vmult(dst.block(1), dst.block(0));
+        dst.block(1).sadd(-1.0, src.block(1)); // tmp.block(1) = -B * dst.block(0) + src.block(1)
+        tmp.block(1) = dst.block(1);
 
-      //Step 4) 
-      F->vmult(yu,yu);
+        // Step 3)
+        // Calculates the second block of src  by inverting negS_matrix.
+        SolverControl solver_control_S(maxit, tol * tmp.block(1).l2_norm());
+        SolverGMRES<TrilinosWrappers::MPI::Vector> solver_S(solver_control_S);
+        solver_S.solve(neg_S, dst.block(1), tmp.block(1), preconditioner_S);
 
-      //Step 5)
-       B_T->vmult(tmp, yp); //tmp(0) = BT*src(1)
-       yu.sadd(-1.0,tmp);
+        // Step 4) 
+        // Scale the primary component by the original F matrix entries.
+        F->vmult(dst.block(0),dst.block(0));
 
-      //Step 6) 
-      yu.scale(diag_D_inv);
-      dst.block(0) = yu;
+        // Step 5)
+        B_T->vmult(tmp.block(0), dst.block(1));
+        //dst.block(0).sadd(-1,tmp.block(0));
+        dst.block(0) -= tmp.block(0);
 
+
+        tmp.block(0) = dst.block(0); //RHS
+        
+        // Step 6)
+        // Finalize the update of the primary component by scaling it with Finv
+        SolverControl solver_control_F1(maxit, tol * tmp.block(0).l2_norm());
+        SolverGMRES<TrilinosWrappers::MPI::Vector> solver_F1(solver_control_F1);
+        solver_F1.solve(*F, dst.block(0), tmp.block(0), preconditioner_F);   
     }
 
   protected:
     const TrilinosWrappers::SparseMatrix *F;
+    const TrilinosWrappers::SparseMatrix *M;
     const TrilinosWrappers::SparseMatrix *B_T;
     const TrilinosWrappers::SparseMatrix *B;
-    const TrilinosWrappers::SparseMatrix *M;
-    TrilinosWrappers::SparseMatrix negative_S;
+    TrilinosWrappers::SparseMatrix neg_S;
 
-    TrilinosWrappers::PreconditionILU preconditionerF;
-    TrilinosWrappers::PreconditionILU preconditionerS;
+    TrilinosWrappers::PreconditionILU preconditioner_F;
+    TrilinosWrappers::PreconditionILU preconditioner_S;
+    
+    TrilinosWrappers::MPI::Vector neg_diag_M_inv;
+    TrilinosWrappers::MPI::Vector diag_M;
 
-    TrilinosWrappers::MPI::Vector diag_D;
-    TrilinosWrappers::MPI::Vector lump_M;
-    TrilinosWrappers::MPI::Vector diag_D_inv;
-    mutable TrilinosWrappers::MPI::Vector tmp;
-    mutable TrilinosWrappers::MPI::Vector tmp2;
-  }; 
-  
+    mutable TrilinosWrappers::MPI::BlockVector tmp;
+   
+  };
   #endif
-  
